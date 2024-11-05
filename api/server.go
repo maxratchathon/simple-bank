8488b010d574ae971df227ddc64851a02b1b0178
Maxmerock123 <Maxmerock@gmail.com>
2024-11-06 05:51:18 +0700

    feat: api POST accounts

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/techschool/simplebank/db/sqlc"
+)
+
+// Sever serves HTTP request for our banking services.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+// Start Runs the HTTP server on a specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
