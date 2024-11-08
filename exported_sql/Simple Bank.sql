9e827d0ecbc248896c5e75ce897f7a8bd7d0e8e9
Maxmerock123 <Maxmerock@gmail.com>
2024-11-08 17:12:06 +0700

    feat: add_user sql db migration

---
+++ exported_sql/Simple Bank.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "account" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint,
+  "amount" bigint NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "tranfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint,
+  "to_account_id" bigint,
+  "amount" bigint,
+  "created_at" timestamp DEFAULT 'now()'
+);
+
+CREATE INDEX ON "account" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "tranfers" ("from_account_id");
+
+CREATE INDEX ON "tranfers" ("to_account_id");
+
+CREATE INDEX ON "tranfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "tranfers"."amount" IS 'must be positive';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "account" ("id");
+
+ALTER TABLE "tranfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "account" ("id");
+
+ALTER TABLE "tranfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "account" ("id");
